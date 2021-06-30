4e6b756256f537169802b395ad56970a208cb19e
Risky <santuyaja019@gmail.com>
2021-06-30 08:30:11 +0700

    Create index.php

---
+++ come_soon/index.php
@@ -0,0 +1,9 @@
+<html><head><title><?php echo "COME SOON";?></title></head>
+<body
+<?php 
+$image_url='https://i.ibb.co/ctfb4Ry/images.jpg';
+?>
+
+<img src="<?php echo $image_url;?>">
+
+<br><h1>GO TO BACK</h1>
