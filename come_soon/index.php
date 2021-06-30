910847fd725e5a0ee134e95eeefcfd49bdb6a1d2
Risky <santuyaja019@gmail.com>
2021-06-30 10:34:57 +0700

    Update index.php

--- come_soon/index.php
+++ come_soon/index.php
@@ -1,6 +1,5 @@
 <html><head><title><?php echo "COME SOON";?></title></head>
 <body
-<a href="https://ibb.co/k0b3NzG"><img src="https://i.ibb.co/k0b3NzG/images.jpg" alt="images" border="0"></a>
-
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/ctfb4Ry/images.jpg" alt="images" border="0"></a>
 
 <br><h1>GO TO BACK</h1>
