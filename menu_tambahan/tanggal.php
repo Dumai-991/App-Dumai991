4d8d2e39fe69331a1e5a5299e9b92ab6cdbc7f9b
Risky [ Zero Tow ] <santuyaja019@gmail.com>
2021-10-14 20:11:55 +0700

    Update tanggal.php

--- menu_tambahan/tanggal.php
+++ menu_tambahan/tanggal.php
@@ -1,12 +1,12 @@
 <?php
 $a = date ("H");
 if (($a>=6) && ($a<=11)){
-echo "<b>, Selamat Pagi !!</b>";
+echo "Selamat Pagi !!";
 }
 else if(($a>11) && ($a<=15))
 {
-echo ", Selamat Pagi !!";}
+echo "Selamat Pagi !!";}
 else if (($a>15) && ($a<=18)){
-echo ", Selamat Siang !!";}
-else { echo ", <b> Selamat Malam </b>";}
+echo "Selamat Siang !!";}
+else { echo "Selamat Malam";}
 ?> 
